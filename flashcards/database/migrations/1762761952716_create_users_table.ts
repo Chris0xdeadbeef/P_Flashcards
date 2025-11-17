7cd8fdfca5ba2b2e2f2e444108ad633ae0749e53
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-11-17 11:18:54 +0100

    feat(migration users): migration pour les users

--- flashcards/database/migrations/1762761952716_create_users_table.ts
+++ flashcards/database/migrations/1762761952716_create_users_table.ts
@@ -6,10 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
-
+      table.string('firstname',255).nullable()
+      table.string('lastname', 255).nullable()
+      table.string('username', 255).notNullable().unique()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 255).notNullable().unique()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
