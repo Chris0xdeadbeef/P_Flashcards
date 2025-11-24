cfa4b1365a1c9d8c5dfbe5fdc6bd02ebb7737a8d
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-11-24 08:58:03 +0100

    chore(migration): users correspondance avec le MCD

--- flashcards/database/migrations/1762761952716_create_users_table.ts
+++ flashcards/database/migrations/1762761952716_create_users_table.ts
@@ -8,11 +8,12 @@ export default class extends BaseSchema {
       table.increments('id').notNullable()
       table.string('firstname',255).nullable()
       table.string('lastname', 255).nullable()
-      table.string('username', 255).notNullable().unique()
+      table.string('username', 255).nullable().unique()
       table.string('email', 255).notNullable().unique()
       table.string('password', 255).notNullable().unique()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
+      table.boolean('is_admin').notNullable().defaultTo(false)
     })
   }
 
