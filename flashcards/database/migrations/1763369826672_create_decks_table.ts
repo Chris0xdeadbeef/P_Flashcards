42c8569417aad8ed8843864fbc8a3e61005ca4ee
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-11-24 08:58:09 +0100

    chore(migration): users correspondance avec le MCD

--- flashcards/database/migrations/1763369826672_create_decks_table.ts
+++ flashcards/database/migrations/1763369826672_create_decks_table.ts
@@ -6,11 +6,18 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name', 255)
-      table.string('description', 255).nullable()
+      table.string('name', 255).notNullable()
+      table.string('description', 255).notNullable()
       table.boolean('published').notNullable().defaultTo(false)
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+      table
+        .integer('user_id') // Clé étrangère
+        .unsigned() // La clé ne doit pas être négative
+        .references('id') // Référence la colonne `id` de la table `section`
+        .inTable('users') // Nom de la table de référence
+        .onDelete('CASCADE') // Supprime les teachers si la section est supprimée
+        .onUpdate('CASCADE') //  Met à jour la clé étrangère si l'id change
     })
   }
 
