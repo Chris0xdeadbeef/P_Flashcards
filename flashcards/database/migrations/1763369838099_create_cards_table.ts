2cc3fa354af7a6b0bb400fd3f18b93c53a5efcf4
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-11-24 08:58:20 +0100

    chore(migration): cards correspondance avec le MCD

--- flashcards/database/migrations/1763369838099_create_cards_table.ts
+++ flashcards/database/migrations/1763369838099_create_cards_table.ts
@@ -8,8 +8,15 @@ export default class extends BaseSchema {
       table.increments('id')
       table.string('question', 255).notNullable()
       table.string('reponse', 255).notNullable()
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+      table
+        .integer('deck_id') // Clé étrangère
+        .unsigned() // La clé ne doit pas être négative
+        .references('id') // Référence la colonne `id` de la table `section`
+        .inTable('decks') // Nom de la table de référence
+        .onDelete('CASCADE') // Supprime les teachers si la section est supprimée
+        .onUpdate('CASCADE') //  Met à jour la clé étrangère si l'id change
     })
   }
 
