3a1ea3b8795e1351571ccc0b88650dacc4f0061d
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-11-24 10:47:00 +0100

    chore(seeder): more decks

--- flashcards/database/seeders/2_deck_seeder.ts
+++ flashcards/database/seeders/2_deck_seeder.ts
@@ -2,8 +2,7 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import Deck from '#models/deck'
 
 export default class DecksSeeder extends BaseSeeder {
-  public async run () {
-
+  public async run() {
     await Deck.createMany([
       {
         name: 'Space Marines – Codex Astartes',
@@ -15,14 +14,56 @@ export default class DecksSeeder extends BaseSeeder {
         name: 'Chaos – Légions Renégates',
         description: 'Les dieux du Chaos, démons et traîtres.',
         userId: 2,
-        published: false,
+        published: true,
       },
       {
         name: 'Xenos – Tyranides',
         description: 'Biomorphes, flotte-ruche, types d’unités.',
         userId: 1,
         published: true,
-      }
+      },
+      {
+        name: 'Orks – Waaagh!',
+        description: 'Les orks, leurs clans et tactiques de combat.',
+        userId: 1,
+        published: true,
+      },
+      {
+        name: 'Eldars – Craftworlds',
+        description: 'Psychologie et armées Eldars.',
+        userId: 2,
+        published: true,
+      },
+      {
+        name: 'Necrons – Dynasties',
+        description: 'Lore et unités Necrons.',
+        userId: 1,
+        published: true,
+      },
+      {
+        name: 'Tau – Empire du Bien Suprême',
+        description: 'Système de castes et unités Tau.',
+        userId: 2,
+        published: true,
+      },
+      {
+        name: 'Imperial Guard – Astra Militarum',
+        description: 'Infanterie, blindés et tactiques de l’IM.',
+        userId: 1,
+        published: true,
+      },
+      {
+        name: 'Inquisition – Ordo Hereticus',
+        description: 'Rôle et hiérarchie des inquisiteurs.',
+        userId: 2,
+        published: true,
+      },
+      {
+        name: 'Sisters of Battle – Adepta Sororitas',
+        description: 'Lore, unités et équipement des SoB.',
+        userId: 1,
+        published: true,
+      },
     ])
   }
 }
