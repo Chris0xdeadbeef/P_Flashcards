79d8a7f55b36dc1d225d67821d6dacd3b326ef6c
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-01 11:28:34 +0100

    feat(routes): modify

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -14,10 +14,14 @@ import router from '@adonisjs/core/services/router'
 router.on('/').render('pages/home')
 router.get('/decks/:deck_id/show', [DecksController, 'show']).as('deck.show')
 router.get('/decks', [DecksController, 'index']).as('decks.index')
+router.get('/deck/:deck_id/edit', [DecksController, 'edit']).as('deck.edit')
+
 router.get('/users', [UsersController, 'index']).as('users.index')
 router.get('/user/show', [UsersController, 'show']).as('users.show')
 
+
 router.delete('/deck/:deck_id/delete', [DecksController, 'destroy']).as('deck.destroy')
+
 
 
 
