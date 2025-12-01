4200ec637dea06ca91107d20b8f5fec72a806b74
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-01 10:30:02 +0100

    refactor(routes): retirer id du chemin

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -15,7 +15,7 @@ router.on('/').render('pages/home')
 router.get('/deck/:id/show', [DecksController, 'show']).as('deck.show')
 router.get('/decks', [DecksController, 'index']).as('decks.index')
 router.get('/users', [UsersController, 'index']).as('users.index')
-router.get('/user/:id/show', [UsersController, 'show']).as('users.show')
+router.get('/user/show', [UsersController, 'show']).as('users.show')
 
 router.delete('/deck/:deck_id/delete', [DecksController, 'destroy']).as('deck.destroy')
 
