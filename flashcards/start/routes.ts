80a1c5141827007bb9cc4db5f3cb59c034e8d457
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-08 09:08:56 +0100

    feat(routes): ajout du PUT

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -12,15 +12,20 @@ import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
 router.get('/decks/:deck_id/show', [DecksController, 'show']).as('deck.show')
 router.get('/decks', [DecksController, 'index']).as('decks.index')
 router.get('/deck/:deck_id/edit', [DecksController, 'edit']).as('deck.edit')
+
+router.put('/deck/:deck_id', [DecksController, 'update']).as('deck.update')
+router.delete('/deck/:deck_id/delete', [DecksController, 'destroy']).as('deck.destroy')
 
 router.get('/users', [UsersController, 'index']).as('users.index')
 router.get('/user/show', [UsersController, 'show']).as('users.show')
 
 
-router.delete('/deck/:deck_id/delete', [DecksController, 'destroy']).as('deck.destroy')
+
+
 
 
 
