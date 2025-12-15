ed96a08ad4506d9a4ea0b1eae1aeec809127df97
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-12-15 10:43:53 +0100

    feat(controller deck): store, show et create

--- flashcards/app/controllers/decks_controller.ts
+++ flashcards/app/controllers/decks_controller.ts
@@ -16,20 +16,42 @@ export default class DecksController {
   /**
    * Display form to create a new record
    */
-  async create({}: HttpContext) {}
+  async create({ view }: HttpContext) {
+    return view.render('pages/decks/createDeck')
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response, session }: HttpContext) {
+    // Valider les données avec deckValidator
+    const validatedData = await request.validateUsing(deckValidator)
+
+    // Créer le deck et lier à l'utilisateur
+    const deck = new Deck()
+    deck.fill(validatedData)
+    deck.userId = 1 // ici user_id = 1, remplacer par auth.user.id si Auth actif
+    deck.published = false // par défaut non publié
+
+    await deck.save()
+
+    // Message flash
+    session.flash({ success: 'Deck créé avec succès.' })
+
+    // Redirection vers la liste des decks de l'utilisateur
+    return response.redirect().toRoute('users.show')
+  }
 
   /**
    * Show individual record
    */
-  async show({ params, view }: HttpContext) {
-    const deck = await Deck.findOrFail(params.deck_id)
+  async show({ view }: HttpContext) {
+    const userId = 1
 
-    return view.render('pages/decks/show', { deck })
+    // Récupérer tous les decks de l'utilisateur
+    const decks = await Deck.query().where('user_id', userId)
+
+    return view.render('pages/decks/show', { decks })
   }
 
   /**
